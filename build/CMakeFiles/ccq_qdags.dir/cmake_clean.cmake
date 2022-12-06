ad17a35ef7f1347c8ac8ed943f726caec1f78dc2
Mati <matilde.rivas@ug.uchile.cl>
2022-12-06 15:00:34 -0300

    no se que es esto

---
+++ build/CMakeFiles/ccq_qdags.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ccq_qdags.dir/queries/join_test.cpp.o"
+  "ccq_qdags"
+  "ccq_qdags.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ccq_qdags.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/qdags.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qdags.dir/queries/j3.cpp.o"
+  "qdags"
+  "qdags.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qdags.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
