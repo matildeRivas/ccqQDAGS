83e7e17f8ee0fd7fa310b7db4e585d7914eb8a65
mati <matilde.rivas@ug.uchile.cl>
2025-04-08 22:16:01 -0400

    Separate parallel yk function

--- src/ghd_optimal_joins.cpp
+++ src/ghd_optimal_joins.cpp
@@ -17,12 +17,29 @@ duration<double> time_span_select;
 
 qdag *yannakakis(ghd root)
 {
-    high_resolution_clock::time_point start, solve_mj, bottom_up, top_down, stop;
-    double total_time = 0.0;
-    duration<double> time_span_mj, time_span_bu, time_span_td, time_span_pp;
-    // Ejecutar multijoin en todos los niveles
-    start = high_resolution_clock::now();
 
+    // Ejecutar multijoin en todos los niveles
+    root.deep_exec_multijoin();
+    auto result = root.get_relations();
+
+    // Ejecutar semijoin entre root y nivel 1
+    root.constrained_by_children();
+
+    root.constrain_children();
+
+    // multijoin entre nodos para obtener resultado del join
+    // todo: obtener todas las relaciones, no solo el siguiente nivel
+    vector<qdag> producto_punto;
+    root.get_subtree_qdags(producto_punto);
+
+    qdag* qResult = multiJoin(producto_punto, false, 1000);
+
+    // high_resolution_clock::time_point start, solve_mj, bottom_up, top_down, stop;
+    // double total_time = 0.0;
+    // duration<double> time_span_mj, time_span_bu, time_span_td, time_span_pp;
+    //  Ejecutar multijoin en todos los niveles
+    // start = high_resolution_clock::now();
+    /*
     // Collect all nodes into a flat list
     std::vector<ghd*> node_list;
     root.collect_all_nodes(node_list);
@@ -33,26 +50,26 @@ qdag *yannakakis(ghd root)
         node_list[i]->exec_multijoin();
     }
 
-
-    //solve_mj = high_resolution_clock::now();
+    // solve_mj = high_resolution_clock::now();
 
     auto result = root.get_relations();
 
     // Ejecutar semijoin entre root y nivel 1
 
-
     root.constrained_by_children();
-    //bottom_up = high_resolution_clock::now();
+    // bottom_up = high_resolution_clock::now();
 
     root.constrain_children();
 
     // multijoin entre nodos para obtener resultado del join
-    //top_down = high_resolution_clock::now();
+    // top_down = high_resolution_clock::now();
     vector<qdag> producto_punto;
     root.get_subtree_qdags(producto_punto);
 
     qdag* qResult = multiJoin(producto_punto, false, 1000);
-    //stop = high_resolution_clock::now();
+    */
+
+    // stop = high_resolution_clock::now();
 
     //time_span_mj = duration_cast<microseconds>(solve_mj - start);
     //total_time = time_span_mj.count();
@@ -74,5 +91,37 @@ qdag *yannakakis(ghd root)
 
     //cout << "final multijoin " << total_time << " seconds" << endl;
 
+    return qResult;
+}
+
+qdag* yannakakis_par(ghd root)
+{
+    // Collect all nodes into a flat list
+    std::vector<ghd*> node_list;
+    root.collect_all_nodes(node_list);
+
+    // Execute exec_multijoin in parallel for all nodes
+#pragma omp parallel for
+    for (size_t i = 0; i < node_list.size(); ++i) {
+        node_list[i]->exec_multijoin();
+    }
+
+    // solve_mj = high_resolution_clock::now();
+
+    auto result = root.get_relations();
+
+    // Ejecutar semijoin entre root y nivel 1
+
+    root.constrained_by_children();
+    // bottom_up = high_resolution_clock::now();
+
+    root.constrain_children();
+
+    // multijoin entre nodos para obtener resultado del join
+    // top_down = high_resolution_clock::now();
+    vector<qdag> producto_punto;
+    root.get_subtree_qdags(producto_punto);
+
+    qdag* qResult = multiJoin(producto_punto, false, 1000);
     return qResult;
 }
