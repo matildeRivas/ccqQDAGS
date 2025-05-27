7c74ebe40b64d33c9ae6f0553b29ac2056414f1f
mati <matilde.rivas@ug.uchile.cl>
2025-05-27 00:40:00 -0400

    j3 without segfault

--- runqueries/runqueries.sh
+++ runqueries/runqueries.sh
@@ -1,23 +1,28 @@
 pattern="j3_ghd"
 params_file="/mnt/c/Users/crist/Documents/query-pattern-finder/${pattern}.txt"
 
-for method in "yk" "yk_par" "mj"
+for method in "yk" "yk_par" # "mj"
 do
     # create output file
     output_file="/mnt/c/Users/crist/Documents/ccqQDAGS/runqueries/outputs/ha_${pattern}_${method}.txt"
     touch $output_file
+    i=1
 
     while IFS= read -r params; do
+        printf "\nRunning ${pattern} - ${method} - ${i}:\n\t${params}\n"
+        ((i++))
+
         # Run the program with a timeout
-        printf "\nRunning ${pattern} - ${method} :\n\t${params}\n"
         timeout 1800 ./build/${pattern} $params $method $output_file
         exit_code=$?
 
         # check errors
         if  [ $exit_code -eq 124 ]; then
             echo "timeout" >> $output_file
+            echo "##### timeout #####"
         elif [ $exit_code -eq 139 ]; then
             echo "segfault" >> $output_file
+            echo "##### segfault #####"
         fi
-    done < $params_file
+    done < ${params_file}
 done
