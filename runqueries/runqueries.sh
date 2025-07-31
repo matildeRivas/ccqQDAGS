f7487f342439a862d872390d89840f80276c404f
mati <matilde.rivas@ug.uchile.cl>
2025-07-30 23:18:45 -0400

    Add original patterns

--- runqueries/runqueries.sh
+++ runqueries/runqueries.sh
@@ -1,28 +1,34 @@
-pattern="bowtie"
-params_file="/mnt/c/Users/crist/Documents/query-pattern-finder/${pattern}.txt"
+#!/bin/bash
+input_folder="patterns"
+output_folder="runqueries/outputs"
 
-for method in "mj" # "yk_par" "yk"
+for pattern in "bowtie" "j3_ghd" "j4_ghd" "ti4_ghd" "triangle_tadpole" "t3_ghd" "t4_ghd" "ti3_ghd"
 do
-    # create output file
-    output_file="/mnt/c/Users/crist/Documents/ccqQDAGS/runqueries/outputs/${pattern}_${method}.txt"
-    touch $output_file
-    i=1
+    params_file="${input_folder}/${pattern}.txt"
 
-    while IFS= read -r params; do
-        printf "\nRunning ${pattern} - ${method} - ${i}:\n\t${params}\n"
-        ((i++))
+    for method in "mj" "yk_par" "yk"
+    do
+        # create output file
+        output_file="${output_folder}/${pattern}_${method}.txt"
+        touch $output_file
+        i=1
 
-        # Run the program with a timeout
-        timeout 1800 ./build/${pattern} $params $method $output_file
-        exit_code=$?
+        while IFS= read -r params; do
+            printf "\nRunning ${pattern}_${method} ${i}:\n\t${params}\n"
+            i=$((i+1))
 
-        # check errors
-        if  [ $exit_code -eq 124 ]; then
-            echo "timeout" >> $output_file
-            echo "##### timeout #####"
-        elif [ $exit_code -eq 139 ]; then
-            echo "segfault" >> $output_file
-            echo "##### segfault #####"
-        fi
-    done < ${params_file}
+            # Run the program with a timeout
+            timeout 1800 ./build/${pattern} $params $method $output_file
+            exit_code=$?
+
+            # check errors
+            if  [ $exit_code -eq 124 ]; then
+                echo "timeout" >> $output_file
+                echo "##### timeout #####"
+            elif [ $exit_code -eq 139 ]; then
+                echo "segfault" >> $output_file
+                echo "##### segfault #####"
+            fi
+        done < ${params_file}
+    done
 done
