6dc459eff74ccee103d767c77155dfdd58dfe300
mati <matilde.rivas@ug.uchile.cl>
2025-08-09 20:46:48 -0400

    Use 2 threads

--- src/ghd_optimal_joins.cpp
+++ src/ghd_optimal_joins.cpp
@@ -51,10 +51,12 @@ qdag* yannakakis_par(ghd root)
     root.collect_all_nodes(node_list);
 
     // Execute exec_multijoin in parallel for all nodes
-#pragma omp parallel for
-    for (size_t i = 0; i < node_list.size(); ++i) {
-        node_list[i]->exec_multijoin();
-    }
+    int j =2;
+    #pragma omp parallel for num_threads(j)
+        for (size_t i = 0; i < node_list.size(); ++i) {
+            node_list[i]->exec_multijoin();
+        }
+    
 
     // solve_mj = high_resolution_clock::now();
 
