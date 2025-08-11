43cf02c57e10d4b802a31070aa9cc2504fc5b9bb
mati <matilde.rivas@ug.uchile.cl>
2025-08-10 22:26:07 -0400

    rerun yk_par

--- runqueries/yk_par.sh
+++ runqueries/yk_par.sh
@@ -19,7 +19,7 @@ ghd_confs=(
     ["ti3_ghd"]="3"
 )
 
-for pattern in "bowtie" # "bowtie" "j3_ghd" "j4_ghd" "t3_ghd" "t4_ghd" "ti3_ghd" "ti4_ghd" "triangle_tadpole"
+for pattern in "j3_ghd" "j4_ghd" "t3_ghd" "t4_ghd" "ti3_ghd" "ti4_ghd"
 do
     params_file="${input_folder}/${pattern}.txt"
     # create output file
