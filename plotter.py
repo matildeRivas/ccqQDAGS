1adb06e7f7f7d17ef7d3c6fcc5773c9a1cc699b9
mati <matilde.rivas@ug.uchile.cl>
2025-08-13 23:18:28 -0400

    plot times

--- plotter.py
+++ plotter.py
@@ -19,7 +19,7 @@ def plot_config():
 
     fig = plt.figure(layout='constrained', figsize=(10, 10))
     axes = fig.subplots(3, 2)
-    fig.suptitle('Times for Different Decompositions in original qdag structure')
+    fig.suptitle('Times for Different Decompositions in high arity qdag')
 
     for i, m in enumerate(multi):
         df1 = pd.read_csv(f"outputs_time/ha_{m}_ghd_yk_1.csv", names=["time"])
@@ -50,35 +50,35 @@ def plot_config():
     plt.savefig("ghd_configurations")
 
 def plot_times():
-    patterns = ["j3_ghd" "t3_ghd" "ti3_ghd" "j4_ghd" "ti4_ghd" "t4_ghd" "triangle_tadpole" "square_tadpole" "bowtie" "triangle_barbell" "square_barbell" "penta_barbell"]
+    patterns = ["J3", "J4", "T3", "Ti3", "T4", "Ti4", "triangle_tadpole", "square_tadpole", "bowtie", "triangle_barbell", "square_barbell", "penta_barbell"]
     medianprops = dict(linestyle='-.', linewidth=2.5, color='black')
 
     fig = plt.figure(layout='constrained', figsize=(10, 10))
-    axes = fig.subplots(4, 2)
+    axes = fig.subplots(6, 2)
     fig.suptitle('Times for High Arity Qdag Variant')
     multi = ["J3", "J4", "T3", "Ti3", "T4", "Ti4"]
 
     for i, p in enumerate(patterns):
         if p in multi:
-            df1 = pd.read_csv(f"ha_outputs_time/{p}_ghd_yk_1.csv", names=["time"])
-            df2 = pd.read_csv(f"ha_outputs_time/{p}_ghd_yk_2.csv", names=["time"])
-            df3 = pd.read_csv(f"ha_outputs_time/{p}_ghd_yk_3.csv", names=["time"])
+            df1 = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_1.csv", names=["time"])
+            df2 = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_2.csv", names=["time"])
+            df3 = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_3.csv", names=["time"])
 
             yk_res = pd.merge(df1, df2, left_index=True, right_index=True, suffixes=('_1', '_2'))
             yk_res = pd.merge(yk_res, df3, left_index=True, right_index=True)
             yk_res["GHD"] = yk_res.min(axis=1)
-            df1p = pd.read_csv(f"ha_outputs_time/{p}_ghd_yk_par_1.csv", names=["time"])
-            df2p = pd.read_csv(f"ha_outputs_time/{p}_ghd_yk_par_2.csv", names=["time"])
-            df3p = pd.read_csv(f"ha_outputs_time/{p}_ghd_yk_par_3.csv", names=["time"])
+            df1p = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_par_1.csv", names=["time"])
+            df2p = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_par_2.csv", names=["time"])
+            df3p = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_par_3.csv", names=["time"])
 
             ykp_res = pd.merge(df1p, df2p, left_index=True, right_index=True)
             ykp_res = pd.merge(ykp_res, df3p, left_index=True, right_index=True)
             ykp_res["GHD par"] = ykp_res.min(axis=1)
-            df_mj = pd.read_csv(f"ha_outputs_time/{p}_ghd_mj.csv", names=["multijoin"])
+            df_mj = pd.read_csv(f"outputs_time/ha_{p}_ghd_mj.csv", names=["multijoin"])
         else:
-            yk_res = pd.read_csv(f"ha_outputs_time/{p}_yk.csv", names=["GHD"])
-            ykp_res = pd.read_csv(f"ha_outputs_time/{p}_yk_par.csv", names=["GHD par"])
-            df_mj = pd.read_csv(f"ha_outputs_time/{p}_mj.csv", names=["multijoin"])
+            yk_res = pd.read_csv(f"outputs_time/ha_{p}_yk.csv", names=["GHD"])
+            ykp_res = pd.read_csv(f"outputs_time/ha_{p}_yk_par.csv", names=["GHD par"])
+            df_mj = pd.read_csv(f"outputs_time/ha_{p}_mj.csv", names=["multijoin"])
 
         df_mj[df_mj['multijoin'] == 'timeout'] = 1800
         df_mj["multijoin"] = pd.to_numeric(df_mj["multijoin"], downcast='float')
