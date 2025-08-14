92b24f830560467f30889910ad3bc1063be11b4b
mati <matilde.rivas@ug.uchile.cl>
2025-08-14 00:22:20 -0400

    parallel yk time comparison

--- plotter.py
+++ plotter.py
@@ -47,7 +47,7 @@ def plot_config():
         for patch, color in zip(props['boxes'], colors):
             patch.set_facecolor(color)
 
-    plt.savefig("ghd_configurations")
+    plt.savefig("outputs/ghd_configurations")
 
 def plot_times():
     patterns = ["J3", "J4", "T3", "Ti3", "T4", "Ti4", "triangle_tadpole", "square_tadpole", "bowtie", "triangle_barbell", "square_barbell", "penta_barbell"]
@@ -104,4 +104,37 @@ def plot_times():
 
     handles, labels = plt.gca().get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper center')
-    plt.savefig("times")
+    plt.savefig("outputs/times")
+
+def yk_times():
+    patterns = ["J3", "J4", "T3", "Ti3", "T4", "Ti4", "triangle_tadpole", "square_tadpole", "bowtie", "triangle_barbell", "square_barbell", "penta_barbell"]
+   
+    multi = ["J3", "J4", "T3", "Ti3", "T4", "Ti4"]
+    data = {"Pattern": [], "Mean difference": [], "Median difference": [], "Max difference": []}
+    df = pd.DataFrame(data)
+    for i, p in enumerate(patterns):
+        if p in multi:
+            df1 = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_1.csv", names=["time"])
+            df2 = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_2.csv", names=["time"])
+            df3 = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_3.csv", names=["time"])
+
+            yk_res = pd.merge(df1, df2, left_index=True, right_index=True, suffixes=('_1', '_2'))
+            yk_res = pd.merge(yk_res, df3, left_index=True, right_index=True)
+            yk_res["GHD"] = yk_res.min(axis=1)
+            df1p = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_par_1.csv", names=["time"])
+            df2p = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_par_2.csv", names=["time"])
+            df3p = pd.read_csv(f"outputs_time/ha_{p}_ghd_yk_par_3.csv", names=["time"])
+
+            ykp_res = pd.merge(df1p, df2p, left_index=True, right_index=True)
+            ykp_res = pd.merge(ykp_res, df3p, left_index=True, right_index=True)
+            ykp_res["GHD par"] = ykp_res.min(axis=1)
+        else:
+            yk_res = pd.read_csv(f"outputs_time/ha_{p}_yk.csv", names=["GHD"])
+            ykp_res = pd.read_csv(f"outputs_time/ha_{p}_yk_par.csv", names=["GHD par"])
+
+        res = pd.merge(yk_res[["GHD"]], ykp_res[["GHD par"]], left_index=True, right_index=True)
+        res["diff"] = 100*(1 - res["GHD par"] / res["GHD"])
+        row = {"Pattern": p, "Mean difference": round(res["diff"].mean(),2), "Median difference": round(res["diff"].median(),2), "Max difference":round(res["diff"].max(),2)}
+        df.loc[len(df)] = row
+    df.to_csv("outputs/ha_percentages.csv", index=False)
+        
